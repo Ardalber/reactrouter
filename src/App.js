95a7f1b752fa956cafe4c3ece835d80be6f26199
Ardalber <[email-du-dveloppeur]cochezgaetan07@gmail.com>
2021-01-07 17:13:58 +0100

    modif de fichier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink, Switch} from "react-router-dom";
 
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Post from "./pages/Post";
+import Author from "./pages/Author";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <NavLink className="nav-link" exact to="/">Accueil</NavLink>
+                <NavLink className="nav-link" to="/blog">Blog</NavLink>
+                <NavLink className="nav-link" to="/me-contacter">Contact</NavLink>
+              </div>
+            </div>
+          </div>
+        </nav>
         <ul>
-          <li><NavLink exact to="/">Accueil</NavLink></li>
-          <li><NavLink to="/mes-articles-de-la-mort">Post</NavLink></li>
-          <li><NavLink to="/me-contacter">Contact</NavLink></li>
+
         </ul>
 
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/mes-articles-de-la-mort" component={Blog} /> */}
+        <Switch>
+          <Route path="/blog/:id" component={Post} />
+          <Route path="/blog" component={Blog} />
+          
 
-        <Route path="/mes-articles-de-la-mort/:bachibouzouk" component={Post} />
+          <Route path="/me-contacter" component={Contact} />
 
-        <Route path="/me-contacter" component={Contact} />
+          <Route path="/auteurs/:id" component={Author} />
+
+          <Route path="/" component={Home} />
+        </Switch>
+
+
+
       </Router>
     )
   }
